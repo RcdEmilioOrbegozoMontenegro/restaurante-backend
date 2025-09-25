138dc36c2c65bd2ddc28b51152b267c70747bc5d
Ricardo Emilio Orbegozo <ricardoemilio744@gmail.com>
2025-09-25 14:49:07 -0500

    4

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -3,6 +3,6 @@ import { setDefaultResultOrder } from "node:dns";
 
 // Fuerza IPv4 ANTES de cargar cualquier otra cosa
 setDefaultResultOrder("ipv4first");
-
+console.log("[bootstrap] dns result order set to ipv4first");
 // Arranca tu server real (usa import dinámico para que lo anterior se aplique)
 await import("./server.js");
